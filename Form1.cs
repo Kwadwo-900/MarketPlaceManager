98ca50afb941eff800bbf3b21f7e13db2ac8f326
Kwadwo-900 <sagudze900@gmail.com>
2022-08-21 02:56:41 +0000

    landing page done

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,30 @@ namespace MarketPlaceManager
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
     }
 }
